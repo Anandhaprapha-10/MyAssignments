6dc7870b7b954a24e96b1ddf76aa30183248307e
Anandhaprapha-10 <131546713+Anandhaprapha-10@users.noreply.github.com>
2023-04-23 14:07:33 +0530

    Add files via upload
    week 1

---
+++ week1/day1/Report.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+public class Report {
+
+	public static void main(String[] args) {
+		
+		Student report = new Student();
+    	report.studentName();	
+    	report.rollNo();
+    	report.collegeName();
+    	report.markScored();
+    	report.cgpa();
+	
+
+	}
+
+}
